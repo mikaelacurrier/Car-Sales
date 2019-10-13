9bf986d948c1db20af36e8220dfdd15ca4490369
Mikaela Currier <mikaela.currier@gmail.com>
2019-10-13 09:39:52 -0400

    Created actions and Reducers for Redux

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+
+export const addFeature = { type: 'ADD_FEATURE', payload }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { reducer } from './reducers';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, rootElement);
